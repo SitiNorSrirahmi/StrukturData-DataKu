2f74797039752a164c4759599da0f66113c0c53d
SitiNorSrirahmi <sitinorsrirahmi@gmail.com>
2024-12-20 20:10:38 +0800

    U: Menu 1 Masukkan Data

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
 #include <conio.h>
+#include <string>
 using namespace std;
+
+struct mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+mahasiswa sikc[30];
+int pos = -1;
+
+void tampilkanSemuaData() {
+    system("cls");
+    if (pos == -1) {
+        cout << "Tidak ada data untuk ditampilkan." << endl;
+    } else {
+        for (int i = 0; i <= pos; i++) {
+            cout << i + 1 << ". " << sikc[i].nim << " , " 
+                 << sikc[i].nama << " , " 
+                 << sikc[i].alamat << " , " 
+                 << sikc[i].ipk << endl;
+        }
+    }
+    getch();
+}
+
+void masukanData() {
+system("cls");
+if (pos >= 29){
+  cout << "\nData sudah penuh." << endl;
+  cin.get(); //menunggu enter ditekan agar kembali ke menu
+        return;    
+}
+
+    pos++;
+    cout << "\nMasukan Data Mu\n";
+    cout << "-----------------\n";
+
+    cout << "Masukan NIM: ";
+    getline(cin, sikc[pos].nim);
+    cout << "Masukan Nama: ";
+    getline(cin, sikc[pos].nama);
+    cout << "Masukan Alamat: ";
+    getline(cin, sikc[pos].alamat);
+    cout << "Masukan IPK: ";
+    cin >> sikc[pos].ipk;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "\nData Berhasil Disimpan!\n";
+    cout << "\nEnter Untuk Kembali ke Menu\n";
+    cin.get();
+    
+}
 
 
 void dMenu(){
@@ -32,10 +86,10 @@ do
   {
    case '1':
     /* code */
-    mPertama("pertama");
+    masukanData();
     break;
    case '2':
-    mPertama("ke- dua");
+    tampilkanSemuaData();
     /* code */ 
     break;  
    case '3':
